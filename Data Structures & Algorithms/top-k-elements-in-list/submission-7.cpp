b44024c210987bfe6d8615f444b5fa16be0e03d3
virgil <1322096+clouds07@users.noreply.github.com>
2026-05-07 11:21:07 +0200

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, int> frequencyMap{};
+        for (auto num : nums) {
+            if (frequencyMap.contains(num)) {
+                frequencyMap[num]++;
+            } else {
+                frequencyMap[num] = 1;
+            }
+        }
+
+        std::multimap<int, int> sortedFrequencyMap{};
+        for (const auto& [k, v] : frequencyMap) {
+            sortedFrequencyMap.insert({v, k});
+            std::cout << k << " " << v << std::endl;
+        }
+
+        vector<int> result;
+        result.reserve(k);
+        auto it = sortedFrequencyMap.crbegin();
+        for (auto i = 0; i < k; ++i) {
+            std::cout << it->first << " " << it->second << std::endl;
+            result.push_back(it->second);
+            it++;
+        }
+        return result;
+    }
+};
