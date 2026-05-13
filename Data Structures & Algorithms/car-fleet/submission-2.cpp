91383b4678547d3315d0328cb70b73cccbe26018
virgil <1322096+clouds07@users.noreply.github.com>
2026-05-13 10:20:07 +0200

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        std::stack<float> times;
+        std::map<int, int> pairs;
+
+        for (int i = 0; i < n; ++i) {
+            pairs.insert(std::pair{position[i], speed[i]});
+        }
+
+        for (auto pair = pairs.rbegin(); pair != pairs.rend(); ++pair) {
+            double arrival = (double)(target - pair->first) / pair->second;
+
+            if ((times.empty() || (arrival > times.top()))) {
+                times.push(arrival);
+            }
+        }
+
+        return times.size();
+    }
+};
